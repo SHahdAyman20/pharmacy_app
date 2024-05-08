bb92aa8371e70030e1054e1d58dac4343e1c3bb6
RanaElsonbaty <ra01210772310@gmail.com>
2024-05-08 22:48:10 +0300

    product_details_screen

--- lib/screens/home_screen/widgets/product_details_screen.dart
+++ lib/screens/home_screen/widgets/product_details_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pharmacy_app/api_model/home_model/product_model.dart';
@@ -127,7 +126,6 @@ class ProductDetails extends StatelessWidget {
                 ))
               ],
             ),
-
         );
       },
     );
